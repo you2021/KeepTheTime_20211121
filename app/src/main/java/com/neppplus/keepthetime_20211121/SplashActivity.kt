24673a4b6992e79501dd3e53b8c5884a4f75c368
null <dbwl232@naver.com>
2021-11-28 13:59:34 +0900

    GlobalData 클래스에, 로그인한 사용자 정보 저장  - 자동 로그인 : 저장된 토큰으로 내 정보 불러오기 시도 => GlobalData의 로그인 유져에 저장 되는가?

--- app/src/main/java/com/neppplus/keepthetime_20211121/SplashActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/SplashActivity.kt
@@ -5,7 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import com.neppplus.keepthetime_20211121.datas.BasicResponse
 import com.neppplus.keepthetime_20211121.utils.ContextUtil
+import com.neppplus.keepthetime_20211121.utils.GlobalDaata
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,9 +32,10 @@ class SplashActivity : BaseActivity() {
 
             val myIntent: Intent
 
-            // 자동로그인 할 상황인지 검사
-            if (ContextUtil.getToken(mContext) == ""){
-                // 토큰 없느 ㄴ상황 : 로그인 필요
+            // 자동로그인 할 상황인지 검사 => 내가 갖고 있는 토큰이, 유혀한 토큰인가?
+            // 내 정보 API 통해서 -> 사용자 정보가 저장되었는가?
+            if (GlobalDaata.loginUser == null){
+                // 토큰 없거나 or 있지만 유효하지 않으면 => 로그인
                 myIntent = Intent(mContext, LoginActivity::class.java)
 
             }else{
@@ -42,6 +50,27 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        testToken()
     }
+
+    fun testToken(){
+        // ContextUtil 에 저장한 토큰이 괜찮은 토큰인가? 검사 -> 내 정보 불러오기. 잘 불러오는지 확인
+        apiService.getRequestMyInfo().enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if(response.isSuccessful){
+                    // 내 정보 가져옴
+                    val br = response.body()!!
+
+                   // GlobalData의 로그인사용자에 사용자 정보
+                    GlobalDaata.loginUser = br.data.user
+                }else{
+                    // 내 정보 불러오기 실패
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+        })
+   }
 }
