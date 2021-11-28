7f51efb6ed88b3b7b58b07a461b9dc23130307aa
null <dbwl232@naver.com>
2021-11-28 13:39:07 +0900

    리싸이클러 어댑터 - 실제 데이터 반영 - inner class의 bind 참수 추가/ 반영

--- app/src/main/java/com/neppplus/keepthetime_20211121/adatpers/MyFriendRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_20211121/adatpers/MyFriendRecyclerAdapter.kt
@@ -4,15 +4,27 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.neppplus.keepthetime_20211121.R
 import com.neppplus.keepthetime_20211121.datas.UserData
+import org.w3c.dom.Text
 import java.util.ArrayList
 
 class MyFriendRecyclerAdapter(val mContext: Context, val mList:List<UserData>) :  RecyclerView.Adapter<MyFriendRecyclerAdapter.MyFriendViewHolder>(){
 
     inner class MyFriendViewHolder(row: View) : RecyclerView.ViewHolder(row){
 
+        val imgProfile = row.findViewById<ImageView>(R.id.imgProfile)
+        val txtNickname = row.findViewById<TextView>(R.id.txtNickname)
+
+        fun bind(data:UserData) {
+
+            txtNickname.text = data.nickname
+            Glide.with(mContext).load(data.profileImageURL).into(imgProfile)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyFriendViewHolder {
@@ -24,6 +36,7 @@ class MyFriendRecyclerAdapter(val mContext: Context, val mList:List<UserData>) :
 
     override fun onBindViewHolder(holder: MyFriendViewHolder, position: Int) {
 
+        holder.bind( mList[position] )
     }
 
     override fun getItemCount(): Int = mList.size
